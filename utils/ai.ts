cc6394deca7388abbf07ab97f2d4085e26425820
towelong <towelong@qq.com>
2024-05-08 18:42:38 +0800

    feat: 修改prompt

--- utils/ai.ts
+++ utils/ai.ts
@@ -10,8 +10,10 @@ const prompt = `
 1. 生成总结：不超过 50 个字，一句话准确的对原文全部内容进行概述。
 2. 生成摘要：准确的对原文全部内容进行概述，字数尽量 **少**
 3. 关键信息列举：明确列出翻译内容的关键信息。
-4. 要求输出结构化 JSON 对象，符合下面 TypeScript：
-interface UserInfo {
+
+# 最终返回结果
+要求输出结构化 JSON 对象，符合下面 TypeScript类型：
+interface SummaryInfo {
     summary: string;
     abstract: string;
     keyPoints: []string;
